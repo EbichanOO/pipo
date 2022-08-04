a5c3cea4bed5ad28ebd598177f68427f4aa18b15
EbichanOO <takanori.sao@icloud.com>
2022-08-04 23:44:19 +0900

    [add]top page but img size is not good

---
+++ react/my-app/src/search.js
@@ -0,0 +1,36 @@
+import searchImg from './search_grass.png';
+import React from 'react';
+
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <div className="Search">
+            <form onSubmit={this.handleSubmit} >
+              <img src={searchImg} className="Search-img" alt="searchGrass" />
+              <label>
+                <input type="text" placeholder="Pipoで検索" value={this.state.value} onChange={this.handleChange} />
+              </label>
+            </form>
+          </div>
+        );
+      }
+}
+
+export default SearchForm;
