3bb18966583c7c275a350d7379aee40f4c06ed34
EbichanOO <takanori.sao@icloud.com>
2022-08-10 07:49:38 +0900

    [add]request for search api

--- go/server.go
+++ go/server.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+    "fmt"
+	"os"
+	
+	"github.com/gin-gonic/gin"
+)
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
@@ -11,10 +20,49 @@ func setupRouter() *gin.Engine {
 }
 
 func searchAPI(c *gin.Context) {
+	searchUseNotionAPI()
 	c.String(200, "search")
 }
 
-func searchUseNotionAPI()
+type RequestNotion struct {
+	Query string `json:"query"`
+}
+
+func searchUseNotionAPI() {
+	jsonString, err := json.Marshal(RequestNotion{Query: "ソフトウェア"})
+	if err != nil {
+        panic("Error")
+    }
+	
+	req, err := http.NewRequest("POST", "https://api.notion.com/v1/search", bytes.NewBuffer(jsonString))
+	if err != nil {
+        panic("Error")
+    }
+
+	f, _ := os.Open("secure")
+	token := make([]byte, 64)
+	n,_ := f.Read(token)
+	req.Header.Set("Authorization", "Bearer "+string(token[:n]))
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Notion-Version", "2022-06-28")
+	req.Header.Add("Content-Type", "application/json")
+	
+	client := new(http.Client)
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err)
+        panic("Error")
+    }
+
+	defer resp.Body.Close()
+
+	byteArray, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        panic("Error")
+    }
+
+    fmt.Printf("%#v", string(byteArray))
+}
 
 func notionOAuthAPI(c *gin.Context) {
 	c.String(200, "Oauth")
