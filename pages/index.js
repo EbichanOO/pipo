409189cb1754a26c719dcbde14f8975113d743de
EbichanOO <takanori.sao@icloud.com>
2022-09-29 18:40:05 +0900

    [add] index page center search box

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Head from 'next/head';
 import SearchForm from '../components/searchbox';
+import styles from '../styles/Home.module.css';
 
 export default function Home(props) {
   
   return (
-    <>
+    <div className={styles.searchpage}>
       <Head>
         <title>PIPO</title>
       </Head>
       <SearchForm />
-    </>
+    </div>
   )
 }
